0d7730c196ec8c459a0d28925dadf3d3c858d7a5
Chad Freeman <ceo@marinadev.xyz>
2025-01-26 13:13:58 -0500

    fix error handling in bcrypt

--- bcrypt/src/lib.rs
+++ bcrypt/src/lib.rs
@@ -1,24 +1,46 @@
-extern  crate bcrypt;
+extern crate bcrypt;
 
 use wasm_bindgen::prelude::*;
 use bcrypt::{DEFAULT_COST, hash, verify };
 
 #[wasm_bindgen]
-pub fn _hash(password: &str) -> Result<String, String> {
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub enum VerifyError {
+    Unknown,
+    InvalidHash
+}
+
+#[wasm_bindgen]
+pub enum HashError {
+    Unknown
+}
+
+#[wasm_bindgen]
+pub fn _hash(password: &str) -> Result<String, HashError> {
     console_error_panic_hook::set_once();
     let password_hash = hash(password, DEFAULT_COST);
     match password_hash {
         Ok(hash) => Ok(hash),
-        Err(error) => Err(error.to_string())
+        Err(_) => Err(HashError::Unknown)
     }
 }
 
 #[wasm_bindgen]
-pub fn _verify(password: &str, hash: &str) -> Result<bool, String> {
+pub fn _verify(password: &str, hash: &str) -> Result<bool, VerifyError> {
     console_error_panic_hook::set_once();
     let is_verified = verify(password, hash);
     match is_verified {
         Ok(value) => Ok(value),
-        Err(error) => Err(error.to_string())
+        Err(error) => {
+            return match error {
+                bcrypt::BcryptError::InvalidHash(_) => Err(VerifyError::InvalidHash),
+                _ => Err(VerifyError::Unknown)
+            }
+        }
     }
 }
